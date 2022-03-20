365c3ef611772d9cc4705f1ebeabde4ad8f2af23
1giba <olamundo@gmail.com>
2022-03-20 16:40:24 -0300

    Move attributes to another interface

--- app/Support/Core/Api/Controllers/Controller.php
+++ app/Support/Core/Api/Controllers/Controller.php
@@ -6,15 +6,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use OpenApi\Attributes as OA;
 
-#[OA\Info(
-    title: "Laravel Apis",
-    version: "1.0.0",
-    description: "A simplest boilerplate for Laravel Apis",
-    contact: new OA\Contact(name: "Gilberto Junior", email: "contact@codehappy.com"),
-    license: new OA\License(name: "MIT")
-)]
 class Controller extends BaseController
 {
     use AuthorizesRequests;
